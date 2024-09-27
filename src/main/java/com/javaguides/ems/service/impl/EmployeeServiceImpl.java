ffcd560c9f87e176ac606871c42931df91c56b8b
leehyunyong89 <leehyunyong89@naver.com>
2024-09-27 13:21:28 +0900

    * Update Employees REST API 구현

--- src/main/java/com/javaguides/ems/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/javaguides/ems/service/impl/EmployeeServiceImpl.java
@@ -39,4 +39,19 @@ public class EmployeeServiceImpl implements EmployeeService {
         return employees.stream().map((employee) -> EmployeeMapper.mapToEmployeeDto(employee)).collect(Collectors.toList());
     }
 
+    @Override
+    public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee) {
+
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                () -> new ResourceNotFoundException("Employee does not exist with given id : " + employeeId));
+
+        employee.setFirstName(updatedEmployee.getFirstName());
+        employee.setLastName(updatedEmployee.getLastName());
+        employee.setEmail(updatedEmployee.getEmail());
+
+        Employee updatedEmployeeObj = employeeRepository.save(employee);
+
+        return  EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
+    }
+
 }
