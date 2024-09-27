1fb67f2566d92d36213ea168d27a92e30ad2753d
leehyunyong89 <leehyunyong89@naver.com>
2024-09-27 11:39:51 +0900

    * Get All Employees REST API 구현

--- src/main/java/com/javaguides/ems/service/EmployeeService.java
+++ src/main/java/com/javaguides/ems/service/EmployeeService.java
@@ -2,8 +2,12 @@ package com.javaguides.ems.service;
 
 import com.javaguides.ems.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
     EmployeeDto createEmployee (EmployeeDto employeeDto);
 
     EmployeeDto getEmployeeById (Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
 }
