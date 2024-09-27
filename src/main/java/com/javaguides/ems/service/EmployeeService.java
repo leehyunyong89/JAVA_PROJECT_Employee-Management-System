96c33aa605344e89f4e55feb9d804bea1c6aa1fa
leehyunyong89 <leehyunyong89@naver.com>
2024-09-27 13:31:56 +0900

    * Delete Employee REST API 구현

--- src/main/java/com/javaguides/ems/service/EmployeeService.java
+++ src/main/java/com/javaguides/ems/service/EmployeeService.java
@@ -12,4 +12,6 @@ public interface EmployeeService {
     List<EmployeeDto> getAllEmployees();
 
     EmployeeDto updateEmployee (Long employeeId, EmployeeDto employeeDto);
+
+    void deleteEmployee (Long employeeId);
 }
--- src/main/java/com/javaguides/ems/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/javaguides/ems/service/impl/EmployeeServiceImpl.java
@@ -54,4 +54,12 @@ public class EmployeeServiceImpl implements EmployeeService {
         return  EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
     }
 
+    @Override
+    public void deleteEmployee(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new ResourceNotFoundException("Employee does not exist with given id : " + employeeId));
+
+        employeeRepository.deleteById(employeeId);
+    }
+
+
 }
